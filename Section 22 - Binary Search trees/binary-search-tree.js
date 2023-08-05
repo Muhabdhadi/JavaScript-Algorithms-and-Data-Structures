4b4ee5b00b168b829b44ccdc7a35f09248844112
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-08-05 14:25:49 +0300

    add binary search tree

---
+++ Section 22 - Binary Search trees/binary-search-tree.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
